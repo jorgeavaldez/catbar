bd713c47b1c732e46c08f4a2cf88afa28c7944c6
Jorge Valdez <valdez.a.jorge@gmail.com>
2015-09-26 18:34:54 -0500

    javascript'

--- templates/main.js
+++ templates/main.js
@@ -1,16 +1,25 @@
 var numClicks = 0;
+var oldNumClicks = 0;
+
 var dt = 1;
+var oldDt = dt;
+
 var spRate = 0;
 
 $("body").keypress(function() {
-	numClicks++;
-
-	spRate = (numClicks / dt).toPrecision(4);
+    oldDt = dt;
+    numClicks++;
 });
 
 setInterval(function() {
 	dt++;
-}, 1000);
 
-$("#counter").text(numClicks);
-$("#rate").text(spRate);
+	spRate = ((numClicks - oldNumClicks) / (dt - oldDt)).toPrecision(4);
+
+  oldNumClicks = numClicks;
+
+  $("#counter").text(oldNumClicks);
+  $("#rate").text(spRate);
+
+}, 80);
+
