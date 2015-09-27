63b0a37cacca2dc92598748caf01dda47cc38b89
Traian-Pop <traian.v.pop@gmail.com>
2015-09-27 01:32:20 -0500

    arduino shit

---
+++ phatcashcats.ino
@@ -0,0 +1,17 @@
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(4, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0) {
+    char data = Serial.read();
+    char str[2];
+    str[0] = data;
+    str[1] = '\0';
+    Serial.print(str);
+  }
+}
